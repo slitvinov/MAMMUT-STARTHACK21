a35e368f5e9d07f77b5610dcd99feb4264d0a4fa
Sergey Litvinov <slitvinov@gmail.com>
2021-03-20 12:44:06 +0100

    up

--- plot
+++ plot
@@ -3,11 +3,16 @@
 set -e
 for i in overhang traverse vertical
 do for h in l r
-   do gnuplot <<!
+   do o=`./origin $i | awk '{print $2}'`
+      v=`./video $i | awk '{print $2}'`
+      gnuplot <<!
       set term png
       set output "$i.$h.png"
       set title "$i($h)"
       set xlabel "seconds"
+      set xtics 120
+      set xtics add ("Origin" $o)
+      set xtics add ("End" $o + $v)
       s = "<./text.py $i.h5 $h"
       plot s u (\$0*0.02):1 w d lt rgb "red" t "AP", \
 	   s u (\$0*0.02):2 w d lt rgb "green" t "UR", \
--- text.py
+++ text.py
@@ -15,11 +15,23 @@ try:
 except OSError:
     sys.stderr.write("explore.py: fail to open '%s'\n" % path)
     sys.exit(2)
-   
-acc = f["acc_LH"]
+
+try:
+    hand = sys.argv.pop(0)
+    acc = f[{"l" : "acc_LH", "r" : "acc_RH"}[hand]]
+except IndexError:
+    sys.stderr.write("explore.py: needs l or r\n")
+    sys.exit(2)
+except KeyError:
+    sys.stderr.write("explore.py: unknown hand '%s'\n" % hand)
+    sys.exit(2)    
 AP = acc["AP"]
 UR = acc["UR"]
 DP = acc["DP"]
 for i in range(AP.size):
-    print(AP[i], UR[i], DP[i])
+    try:
+        print("%.16e %.16e %.16e" % (AP[i], UR[i], DP[i]))
+    except BrokenPipeError:
+        sys.exit(0)
+        
 f.close()
